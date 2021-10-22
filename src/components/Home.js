2703e2c0685a0b29fab4b31264b48942241428f8
Anvesh Dange <anveshdange19@gmail.com>
2021-10-22 10:20:52 +0530

    Tesla App First Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import Section from './Section'
+
 function Home() {
     return (
+        
         <Container>
            <Section  
            title='Model S' 
@@ -55,6 +57,8 @@ function Home() {
            rightBtnText = ''
            />
         </Container>
+        
+        
     )
 }
 
